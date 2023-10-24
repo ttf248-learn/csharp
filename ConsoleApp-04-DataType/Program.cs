b41a4d1bac97312c2f82a0945e9c499426996259
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-24 21:11:54 +0800

    ConsoleApp-04-DataType

---
+++ ConsoleApp-04-DataType/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ConsoleApp_04_DataType
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("size of int: {0}", sizeof(int));
+            Console.ReadLine(); 
+        }
+    }
+}
