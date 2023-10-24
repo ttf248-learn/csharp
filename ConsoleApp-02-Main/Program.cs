86ac7e5f0a20cc55597cbad6f7d1bebcc5c2f2a4
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-24 16:40:58 +0800

    ConsoleApp-02-Main

---
+++ ConsoleApp-02-Main/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConsoleApp_02_Main
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
