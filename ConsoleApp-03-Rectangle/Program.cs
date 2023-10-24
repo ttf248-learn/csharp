ebbace0ee17c54c1a707a3bfebf65e16998d1422
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-24 21:09:56 +0800

    ConsoleApp-03-Rectangle

---
+++ ConsoleApp-03-Rectangle/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp_03_Rectangle
+{
+    class Rectangle
+    {
+        // 成员变量
+        double length;
+        double width;
+
+        public void Acceptdetails()
+        {
+            length = 4.5;
+            width = 3.5;
+        }
+
+        public double GetArea()
+        {
+            return length * width;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("Length: {0}", length);
+            Console.WriteLine("Width: {0}", width);
+            Console.WriteLine("Area: {0}", GetArea());
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle();
+
+            r.Acceptdetails();
+            r.Display();
+
+            Console.ReadLine();
+        }
+    }
+}
