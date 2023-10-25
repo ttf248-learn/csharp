5814f82016b929d20eb44d642b1058bbe8b960a0
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-25 15:09:25 +0800

    下拉框背景色

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,28 +1,59 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.ComponentModel;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
-namespace WpfApp
+namespace YearComboBox
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+            DataContext = new YearComboBoxViewModel();
         }
+    }
+
+    public class YearComboBoxViewModel : INotifyPropertyChanged
+    {
+        public YearComboBoxViewModel()
+        {
+            // 初始化下拉框数据
+            Years = new List<YearItem>();
+            int currentYear = DateTime.Now.Year;
+
+            for (int i = currentYear - 10; i <= currentYear + 10; i++)
+            {
+                Years.Add(new YearItem { Year = i });
+            }
+
+            SelectedYear = Years[10]; // 初始选择当前年份
+        }
+
+        private YearItem selectedYear;
+
+        public YearItem SelectedYear
+        {
+            get { return selectedYear; }
+            set
+            {
+                selectedYear = value;
+                OnPropertyChanged("SelectedYear");
+            }
+        }
+
+        public List<YearItem> Years { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+
+    public class YearItem
+    {
+        public int Year { get; set; }
     }
 }
