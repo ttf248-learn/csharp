67f461b87d76980853865fc943f5eea9a73f304b
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-26 22:17:41 +0800

    DXApplicationDataEdit

---
+++ DXApplicationDataEdit/ViewModels/MainViewModel.cs
@@ -0,0 +1,30 @@
+﻿using DevExpress.Mvvm.CodeGenerators;
+using System;
+
+namespace DXApplicationDataEdit.ViewModels
+{
+    [GenerateViewModel]
+    public partial class MainViewModel
+    {
+        [GenerateProperty]
+        string _Status;
+        [GenerateProperty]
+        string _UserName;
+
+        [GenerateCommand]
+        void Login() => Status = "User: " + UserName;
+        bool CanLogin() => !string.IsNullOrEmpty(UserName);
+
+        private DateTime? _currentYear = DateTime.Today;
+
+        public DateTime? CurrentYear
+        {
+            get => _currentYear;
+            set
+            {
+                Console.WriteLine("current year:{0}", _currentYear.Value.Year);
+                _currentYear = value;
+            }
+        }
+    }
+}
