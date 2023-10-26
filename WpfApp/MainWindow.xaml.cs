7205d6f088839f7c658d65e2fece88bb7016f70e
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-26 15:41:31 +0800

    辅助函数，定位日历组件的每个日期 button

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Media;
 
 namespace CalendarApp
 {
@@ -41,7 +44,67 @@ namespace CalendarApp
             {
                 var monthCalendar = CalendarItems.Items[i] as Calendar;
                 monthCalendar.DisplayDate = new DateTime(selectedYear, i + 1, 1);
+
+                // 自定义日期的背景色
+                for (int day = 1; day <= DateTime.DaysInMonth(selectedYear, i + 1); day++)
+                {
+                    DateTime date = new DateTime(selectedYear, i + 1, day);
+                    CalendarDayButton dayButton = GetCalendarDayButton(monthCalendar, date);
+                    if (dayButton != null)
+                    {
+                        if (date.DayOfWeek == DayOfWeek.Saturday || date.DayOfWeek == DayOfWeek.Sunday)
+                        {
+                            // 周末，背景色为白色 (D)
+                            dayButton.Background = Brushes.White;
+                        }
+                        else
+                        {
+                            // 周一到周五，背景色为蓝色 (A)
+                            dayButton.Background = Brushes.LightBlue;
+                        }
+                    }
+                }
             }
         }
+
+        // 获取指定日期对应的 CalendarDayButton
+        private CalendarDayButton GetCalendarDayButton(Calendar calendar, DateTime date)
+        {
+            CalendarDayButton dayButton = null;
+
+            // 找到 Calendar 控件的日历视觉树根元素
+            FrameworkElement calendarRoot = (FrameworkElement)calendar.Template.FindName("PART_Root", calendar);
+
+            // 查找指定日期对应的 CalendarDayButton
+            foreach (var child in GetVisualChildren(calendarRoot))
+            {
+                if (child is CalendarDayButton dayButtonElement)
+                {
+                    if (dayButtonElement.DataContext is DateTime dayDate && dayDate.Date == date.Date)
+                    {
+                        dayButton = dayButtonElement;
+                        break;
+                    }
+                }
+            }
+
+            return dayButton;
+        }
+
+        // 辅助方法：获取视觉树中的所有子元素
+        private static IEnumerable<DependencyObject> GetVisualChildren(DependencyObject parent)
+        {
+            int childCount = VisualTreeHelper.GetChildrenCount(parent);
+            for (int i = 0; i < childCount; i++)
+            {
+                yield return VisualTreeHelper.GetChild(parent, i);
+
+                foreach (var childOfChild in GetVisualChildren(VisualTreeHelper.GetChild(parent, i)))
+                {
+                    yield return childOfChild;
+                }
+            }
+        }
+
     }
 }
