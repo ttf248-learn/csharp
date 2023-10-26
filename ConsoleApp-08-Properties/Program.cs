c4cc0b402a56fcba4f3cbfc3286b4602ec2c8c25
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-26 19:50:42 +0800

    ConsoleApp-08-Properties

---
+++ ConsoleApp-08-Properties/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace ConsoleApp_08_Properties
+{
+    public class  Person
+    {
+        // 声明属性
+        private string name;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (!string.IsNullOrEmpty(value))
+                {
+                    name = value;
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person person = new Person();
+
+            person.Name = "Test";
+
+            string name = person.Name;
+
+            Console.WriteLine("Name:{0}", name);
+        }
+    }
+}
