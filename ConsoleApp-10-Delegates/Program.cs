63cb23df96a0b81275dd3296e1c34154b710caf0
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-31 16:20:04 +0800

    ConsoleApp-11-Lambda

--- ConsoleApp-10-Delegates/Program.cs
+++ ConsoleApp-10-Delegates/Program.cs
@@ -35,3 +35,7 @@ namespace ConsoleApp_09_Delegates
         }
     }
 }
+
+//这个示例演示了如何使用委托来引用不同的方法。在这里，我们定义了一个 Calculator 类，其中包含了 Add 和 Subtract 两个方法，然后创建了一个委托 BinaryOperation，用于引用这两个方法。
+
+//我们创建了委托实例 add 和 subtract，并分别将它们分配给 Add 和 Subtract 方法。然后，我们可以使用这些委托实例来调用相应的方法，从而实现动态方法调用。
