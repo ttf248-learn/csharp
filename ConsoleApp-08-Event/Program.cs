1ab835a2ed6c5863b38848606f3a24957a994b18
tianlong.xiang <tianlong.xiang@foxmail.com>
2023-10-31 15:43:47 +0800

    ConsoleApp-08-Event

---
+++ ConsoleApp-08-Event/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ConsoleApp_08_Event
+{
+    public class Button
+    {
+        // 定义事件
+        public event EventHandler Clicked;
+
+        public void OnClick()
+        {
+            // 触发事件
+            Clicked?.Invoke(this, EventArgs.Empty);
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Button button = new Button();
+
+            // 订阅事件
+            button.Clicked += (sender, e) =>
+            {
+                Console.WriteLine("Button Clicked: {0}", sender);
+            };
+
+            button.Clicked += (sender, e) =>
+            {
+                Console.WriteLine("Button 2 Clicked: {0}", sender);
+            };
+
+            // 模拟按钮点击
+            button.OnClick();
+        }
+    }
+}
