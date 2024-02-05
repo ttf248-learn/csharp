99698f16bf6b55c09a013bf034e8f4a857d61511
tianlong.xiang <tianlong.xiang@foxmail.com>
2024-02-05 21:14:49 +0800

    DXApplicationGridControl

---
+++ DXApplicationGridControl/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using DevExpress.Xpf.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+
+namespace DXApplicationGridControl
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : ThemedWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += MainWindow_Loaded;
+        }
+
+        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
+        {
+            // 添加测试数据
+            ObservableCollection<Person> persons = new ObservableCollection<Person>
+            {
+                new Person { ID = 1, Name = "John", Age = 30 },
+                new Person { ID = 2, Name = "Jane", Age = 25 },
+                new Person { ID = 3, Name = "Bob", Age = 35 }
+            };
+
+            gridControl.ItemsSource = persons;
+        }
+    }
+
+    public class Person
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
